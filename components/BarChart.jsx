57ab267559490cdafb69edfc3c2188b83da3f1b9
JP <jpberrocal@gmail.com>
2024-05-17 15:38:11 -0600

    Recent orders started

--- components/BarChart.jsx
+++ components/BarChart.jsx
@@ -12,10 +12,47 @@ ChartJS.register(
 );
 
 export const BarChart = () => {
+
+    const [chartData, setChartData] = useState({
+        datasets: []
+    });
+
+    const [chartOptions, setChartOptions] = useState({});
+
+    useEffect(() => {
+        setChartData({
+            labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+            datasets: [
+                {
+                    label: 'Sales $',
+                    data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
+                    borderColor: 'rgb(53, 162, 235)',
+                    backgroundColor: 'rgb(53, 162, 235, 0.4',
+                },
+            ]
+        })
+        setChartOptions({
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Daily Revenue'
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+        })
+
+    }, [])
+
+
+
     return (
         <>
             <div className='w-full md:col-span-2 lg:h-[70vh] h-[50hv] m-auto p-4 border rounded-lg bg-white'>
-
+                <Bar data={chartData} options={chartOptions} />
             </div>
         </>
     )
